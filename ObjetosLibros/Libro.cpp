0bb19329fd6b58b0ef9adea58293d975f32ea16d
Juan Pablo de León Miranda <JuanPablodeLeon>
2025-09-24 13:02:43 -0600

    Objeto Libro

---
+++ ObjetosLibros/Libro.cpp
@@ -0,0 +1,68 @@
+//
+// Created by juanp on 23/09/2025.
+//
+
+#include "Libro.h"
+
+Libro::Libro() {}
+
+Libro::~Libro() {}
+
+Libro::Libro(std::string titulo, std::string ISBN, std::string genero, std::string fecha, std::string autor)
+{
+    this->titulo = titulo;
+    this->ISBN = ISBN;
+    this->genero = genero;
+    this->fecha = fecha;
+    this->autor = autor;
+}
+
+std::string Libro::getTitulo()
+{
+    return this->titulo;
+}
+
+std::string Libro::getISBN()
+{
+    return this->ISBN;
+}
+
+std::string Libro::getGenero()
+{
+    return this->genero;
+}
+
+std::string Libro::getFecha()
+{
+    return this->fecha;
+}
+
+std::string Libro::getAutor()
+{
+    return this->autor;
+}
+
+void Libro::setTitulo(std::string titulo)
+{
+    this->titulo = titulo;
+}
+
+void Libro::setISBN(std::string ISBN)
+{
+    this->ISBN = ISBN;
+}
+
+void Libro::setGenero(std::string genero)
+{
+    this->genero = genero;
+}
+
+void Libro::setFecha(std::string fecha)
+{
+    this->fecha = fecha;
+}
+
+void Libro::setAutor(std::string autor)
+{
+    this->autor = autor;
+}
---
+++ ObjetosLibros/Libro.h
@@ -0,0 +1,37 @@
+//
+// Created by juanp on 23/09/2025.
+//
+
+#ifndef EDD_PROYECTO1_202031101_LIBRO_H
+#define EDD_PROYECTO1_202031101_LIBRO_H
+#include <string>
+
+class Libro
+{
+private:
+    std::string titulo;
+    std::string ISBN;
+    std::string genero;
+    std::string fecha;
+    std::string autor;
+
+public:
+    Libro();
+    ~Libro();
+    Libro(std::string titulo, std::string ISBN, std::string genero, std::string fecha, std::string autor);
+
+    std::string getTitulo();
+    std::string getISBN();
+    std::string getGenero();
+    std::string getFecha();
+    std::string getAutor();
+
+    void setTitulo(std::string titulo) ;
+    void setISBN(std::string ISBN);
+    void setGenero(std::string genero);
+    void setFecha(std::string fecha);
+    void setAutor(std::string autor);
+};
+
+
+#endif //EDD_PROYECTO1_202031101_LIBRO_H
